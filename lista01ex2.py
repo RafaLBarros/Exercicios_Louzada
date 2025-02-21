50e5ae532432b861254e70a032c89662a6342068
RafaLBarros <64503929+RafaLBarros@users.noreply.github.com>
2025-02-21 19:28:04 -0300

    Add files via upload

---
+++ lista01ex2.py
@@ -0,0 +1,3 @@
+tempC = float(input("Digite a temperatura em Celsius: "))
+tempF = (tempC * (9/5)) + 32
+print("A temperatura em Fahreinheit é ",tempF)
---
+++ lista01ex4.py
@@ -0,0 +1,4 @@
+horas = int(input("Digite o número de horas trabalhadas: "))
+valor = float(input("Digite o valor recebido por hora: "))
+salario = horas * valor
+print("Seu salário mensal é ",salario)
