8c94f7e6cc188366f522b72912cf0c1ea2ec64e6
RafaLBarros <64503929+RafaLBarros@users.noreply.github.com>
2025-02-21 20:10:07 -0300

    Delete lista01ex4.py

--- lista01ex4.py
+++
@@ -1,4 +0,0 @@
-horas = int(input("Digite o número de horas trabalhadas: "))
-valor = float(input("Digite o valor recebido por hora: "))
-salario = horas * valor
-print("Seu salário mensal é ",salario)
